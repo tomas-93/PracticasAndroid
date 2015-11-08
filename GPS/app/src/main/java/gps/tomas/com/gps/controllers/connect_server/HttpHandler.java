061ad233c0764ac7e6d2cbd18b58a7a3af7af875
Tomas Yussef Galicia Guzman <tomasyussef@gmail.com>
2015-11-08 14:15:31 -0600

    ..

--- GPS/app/src/main/java/gps/tomas/com/gps/controllers/connect_server/HttpHandler.java
+++
@@ -1,8 +0,0 @@
-package gps.tomas.com.gps.controllers.connect_server;
-
-/**
- * Created by Tomas on 19/09/2015.
- */
-public class HttpHandler
-{
-}
--- GPS/app/src/main/java/gps/tomas/com/gps/controllers/gps/GpsHandler.java
+++
@@ -1,9 +0,0 @@
-package gps.tomas.com.gps.controllers.gps;
-
-/**
- * Created by Tomas on 19/09/2015.
- */
-public class GpsHandler
-{
-
-}
